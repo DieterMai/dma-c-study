55838eb4f257f76f37b1193175ad93f0eda0ccdd
Dieter Mai <maidieter@googlemail.com>
2022-10-01 17:39:48 +0200

    Page 37

--- cards.c
+++ cards.c
@@ -3,27 +3,44 @@
 
 int main(){
 	char card_name[3];
-	puts("Enter the card name: ");
-	scanf("%2s", card_name);
-	int val = 0;
+	int count = 0;
 	
-	switch(card_name[0]){
-	case 'K':
-	case 'J':
-	case 'A':
-		val = 10;
-		break;
-	default:
-		val = atoi(card_name);
-		break;
-	}
+	while(card_name[0] != 'X'){
+		puts("Enter the card name: ");
+		scanf("%2s", card_name);
 		
-	if( val >= 10){
-		puts("Count has gone up");
-	}else if( val <= 6){
-		puts("Count has gone down");
+		int val = 0;
+		
+		switch(card_name[0]){
+		case 'K':
+		case 'J':
+		case 'A':
+			val = 10;
+			break;
+		case 'X':
+			puts("Quit card game");
+			continue;
+		default:
+			val = atoi(card_name);
+			if( val < 2 ){
+				puts("Invalid input");				
+				continue;
+			}
+			break;
+		}
+			
+		if( val >= 10){
+			count++;
+			puts("Count has gone up");
+
+		}else if( val <= 6){
+			count--;
+			puts("Count has gone down");
+		}
+		
+		printf("Count is at %i\n", count);
+		printf("The card value is:  %i\n", val);	
 	}
-	
-	printf("The card value is:  %i\n", val);
+
 	return 0;
 }
