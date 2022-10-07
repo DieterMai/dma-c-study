1243421889cc510cf162ee82aaf45fd9906bfc0c
Dieter Mai <dma@sysgo.com>
2022-10-07 12:54:19 +0200

    Page 52 (92)

---
+++ head-first-c/southest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+void goSouthEast(int *lat, int *lon){
+	*lat = *lat - 1;
+	*lon = *lon + 1;
+}
+
+int main(){
+	int latitude = 32;
+	int longitude = -64;
+	
+	goSouthEast(&latitude, &longitude);
+	
+	printf("Avest! now at [%i, %i]\n", latitude, longitude);
+
+	return 0;
+}
